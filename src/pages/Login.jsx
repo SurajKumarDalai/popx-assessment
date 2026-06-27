616f1610f05b3ec5efc323562092e7759edb3f84
SurajKumarDalai <surajkumardalaiwork@gmail.com>
2026-06-27 15:15:15 +0530

    Implement login page and complete PopX assessment

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,70 @@
+import "./Login.css";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 function Login() {
-    return <h1>Login Page</h1>;
-  }
-  
-  export default Login;
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleLogin = () => {
+    const user = JSON.parse(localStorage.getItem("userData"));
+
+    if (
+      user &&
+      user.email === email &&
+      user.password === password
+    ) {
+      navigate("/profile");
+    } else {
+      setError("Invalid email or password");
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <h1 className="login-title">
+        Signin to your <br />
+        PopX account
+      </h1>
+
+      <p className="login-subtitle">
+        Lorem ipsum dolor sit amet,
+        <br />
+        consectetur adipiscing elit.
+      </p>
+
+      <div className="form-group">
+        <label>Email Address</label>
+
+        <input
+          type="email"
+          placeholder="Enter email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+
+      <div className="form-group">
+        <label>Password</label>
+
+        <input
+          type="password"
+          placeholder="Enter password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+
+      <button className="login-btn" onClick={handleLogin}>
+        Login
+      </button>
+
+      {error && <p className="error">{error}</p>}
+    </div>
+  );
+}
+
+export default Login;
