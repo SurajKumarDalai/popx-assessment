63cc7af10ad0fccd2329ebbd0f97e1362cdd803f
SurajKumarDalai <surajkumardalaiwork@gmail.com>
2026-06-27 14:37:42 +0530

    Build Welcome page UI and navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+
 import Welcome from "./pages/Welcome";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
 
 function App() {
-  return <Welcome />;
+  return (
+    <Routes>
+      <Route path="/" element={<Welcome/>} />
+      <Route path="/signup" element={<Signup/>} />
+      <Route path="/login" element={<Login/>} />
+      <Route path="/profile" element={<Profile/>} />
+    </Routes>
+  );
 }
 
 export default App;
--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,9 +1,34 @@
+import "./Welcome.css";
+import { useNavigate } from "react-router-dom";
+
 function Welcome() {
-    return (
-      <div>
-        <h1>Welcome Page</h1>
-      </div>
-    );
-  }
-  
-  export default Welcome;
+  const navigate = useNavigate();
+
+  return (
+    <div className="welcome-container">
+      <h1 className="welcome-title">Welcome to PopX</h1>
+
+      <p className="welcome-description">
+        Lorem ipsum dolor sit amet,
+        <br />
+        consectetur adipiscing elit.
+      </p>
+
+      <button
+        className="primary-btn"
+        onClick={() => navigate("/signup")}
+      >
+        Create Account
+      </button>
+
+      <button
+        className="secondary-btn"
+        onClick={() => navigate("/login")}
+      >
+        Already Registered? Login
+      </button>
+    </div>
+  );
+}
+
+export default Welcome;
